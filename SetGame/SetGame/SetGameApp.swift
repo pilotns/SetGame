7f8d4024f0b3c04de2e2d6c1d2d98534cbf18236
pilotns <pilot.ua@gmail.com>
2023-04-07 12:29:07 +0300

    Project created

---
+++ SetGame/SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by pilotns on 07.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
