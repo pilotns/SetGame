fd4b4d0152c6aa3037ad091a0a935f80883f0b13
pilotns <pilot.ua@gmail.com>
2023-04-12 10:31:55 +0300

    GameControlView
    Refactor using Geometry feature

--- SetGame/SetGame/Deck/Views/SetGameDeskView.swift
+++ SetGame/SetGame/Deck/Views/SetGameDeskView.swift
@@ -16,8 +16,8 @@ struct SetGameDeskView: View {
     var body: some View {
         ZStack {
             ScrollView {
-                WithViewStore(self.store, observe: \.dealt) { viewStore in
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+                WithViewStore(self.store, observe: \.dealt) { deck in
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
                         ForEachStore(self.store.scope(state: \.dealt, action: Deck.Action.card(id:action:))) { card in
                             WithViewStore(card, observe: \.id) { id in
                                 CardView(store: card)
@@ -29,16 +29,63 @@ struct SetGameDeskView: View {
                             }
                         }
                     }
-                    .animation(.default, value: viewStore.state)
+                    .animation(.default, value: deck.state)
                     .padding(.horizontal)
                 }
             }
         }
         .safeAreaInset(edge: .bottom) {
-            GameControlView(store: self.store, namespace: dealing)
-                .frame(maxHeight: 140)
+            WithViewStore(self.store, observe: \.geometry) { geometryStore in
+                ZStack(alignment: .top) {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                    
+                    Color.clear
+                        .contentShape(Rectangle())
+                        .frame(maxHeight: 15)
+                        .overlay {
+                            Capsule()
+                                .frame(maxWidth: 30, maxHeight: 5)
+                                .foregroundColor(.gray)
+                        }
+                        .gesture(dragGesture(geometryStore))
+                    
+                    GameControlView(store: self.store, namespace: dealing)
+                }
+                .background(.ultraThinMaterial)
+                .frame(height: geometryStore.state.gameControlHeight)
+                .offset(geometryStore.state.gameControlOffset)
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
+    }
+    
+    @State private var previousTranslation: CGSize = .zero
+    
+    private func dragGesture(_ geometryStore: ViewStore<Geometry.State, Deck.Action>) -> some Gesture {
+        DragGesture(minimumDistance: 0, coordinateSpace: .global)
+            .onChanged { value in
+                let translationAmount = value.translation.height - previousTranslation.height
+                let newHeight: CGFloat
+                
+                let currentHeight = geometryStore.state.gameControlHeight
+                
+                if currentHeight - translationAmount < Constants.GameControl.height {
+                    newHeight = currentHeight - translationAmount * 0.2
+                } else {
+                    newHeight = currentHeight - translationAmount
+                }
+                
+                geometryStore.send(.geometry(.updateGameControlHeight(newHeight)))
+                previousTranslation = value.translation
+            }
+            .onEnded { value in
+                previousTranslation = .zero
+                let currentHeight = geometryStore.state.gameControlHeight
+                _ = currentHeight < Constants.GameControl.height
+                ? geometryStore.send(.geometry(.resetGameControlHeight))
+                : geometryStore.send(.geometry(.updateGameControlHeight(currentHeight)))
+            }
     }
 }
 
